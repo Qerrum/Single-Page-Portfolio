008c13f41a63bdbd77e3208e20a0bbfd416faa7c
Qerrum Laptop <official.qerrum@gmail.com>
2019-09-06 22:49:20 +0200

    first commit

---
+++ js/js.js
@@ -0,0 +1,104 @@
+	// Get the modal
+	var modal = document.getElementById('myModal');
+	var img = document.getElementById('myImg');
+	var modalImg = document.getElementById("img01");
+	var captionText = document.getElementById("caption");
+	img.onclick = function(){
+		modal.style.display = "block";
+		modalImg.src = this.src;
+		
+	}
+	var span = document.getElementsByClassName("close")[0];
+
+	span.onclick = function() { 
+		modal.style.display = "none";
+	}
+	
+	
+	
+	// Get the modal2
+	var modal2 = document.getElementById('myModal2');
+	var img2 = document.getElementById('myImg2');
+	var modalImg2 = document.getElementById("img02");
+	
+	img2.onclick = function(){
+		modal2.style.display = "block";
+		modalImg2.src = this.src;
+	
+	}
+	var span2 = document.getElementsByClassName("close2")[0];
+	span2.onclick = function() { 
+		modal2.style.display = "none";
+	}
+	
+	
+	// Get the modal3
+	var modal3 = document.getElementById('myModal3');
+
+	
+	var img3 = document.getElementById('myImg3');
+	var modalImg3 = document.getElementById("img03");
+	
+	img3.onclick = function(){
+		modal3.style.display = "block";
+		modalImg3.src = this.src;
+	
+	}
+	var span3 = document.getElementsByClassName("close3")[0];
+	span3.onclick = function() { 
+		modal3.style.display = "none";
+	}
+	
+	
+	// Get the modal4
+	var modal4 = document.getElementById('myModal4');
+
+	
+	var img4 = document.getElementById('myImg4');
+	var modalImg4 = document.getElementById("img04");
+	
+	img4.onclick = function(){
+		modal4.style.display = "block";
+		modalImg4.src = this.src;
+	
+	}
+	var span4 = document.getElementsByClassName("close4")[0];
+	span4.onclick = function() { 
+		modal4.style.display = "none";
+	}
+	
+	
+	// Get the modal5
+	var modal5 = document.getElementById('myModal5');
+
+	
+	var img5 = document.getElementById('myImg5');
+	var modalImg5 = document.getElementById("img05");
+	
+	img5.onclick = function(){
+		modal5.style.display = "block";
+		modalImg5.src = this.src;
+	
+	}
+	var span5 = document.getElementsByClassName("close5")[0];
+	span5.onclick = function() { 
+		modal5.style.display = "none";
+	}
+	
+	
+	// Get the modal6
+	var modal6 = document.getElementById('myModal6');
+
+	
+	var img6 = document.getElementById('myImg6');
+	var modalImg6 = document.getElementById("img06");
+	
+	img6.onclick = function(){
+		modal6.style.display = "block";
+		modalImg6.src = this.src;
+	
+	}
+	var span6 = document.getElementsByClassName("close6")[0];
+	span6.onclick = function() { 
+		modal6.style.display = "none";
+	}
